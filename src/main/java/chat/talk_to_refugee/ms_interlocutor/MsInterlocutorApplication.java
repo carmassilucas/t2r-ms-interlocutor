edf8785f151c72dd10ea01488800fb995474f7d7
Lucas Carmassi <carmassilucas@gmail.com>
2025-05-25 10:16:01 -0300

    projeto spring base e conexao com db

---
+++ src/main/java/chat/talk_to_refugee/ms_interlocutor/MsInterlocutorApplication.java
@@ -0,0 +1,13 @@
+package chat.talk_to_refugee.ms_interlocutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInterlocutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInterlocutorApplication.class, args);
+	}
+
+}
